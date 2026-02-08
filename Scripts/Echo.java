108560cb73b466c19cfa3ffec04ab11e803e2562
Chuck-Cartledge <github02@clc-ent.com>
2026-02-08 11:30:09 -0500

    Delete Scripts directory
    Cleaning up errors by students

--- Scripts/Echo.java
+++
@@ -1,9 +0,0 @@
-public class Echo {
-    public static void main(String[] args) {
-        System.out.println("Arguments received:");
-        // Loop through all elements in the args array
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("Argument " + (i + 1) + ": " + args[i]);
-        }
-    }
-}
