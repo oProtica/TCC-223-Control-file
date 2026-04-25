2932c2b25520bba62802f2f4ad69ffc6749aa03e
Thomas Sigmon <thomas@oprotica.xyz>
2026-04-25 08:21:03 -0400

    Added controller to help manage state and execution without cluttering Main.

---
+++ src/CRMController.java
@@ -0,0 +1,49 @@
+public class CRMController {
+    private String DBFileName;
+    private CRMState state;
+    private CRMParser parser;
+
+    public CRMController() {
+        this.parser = new CRMParser(this);
+    }
+
+    // state related methods
+    public CRMState getState() {
+        return state;
+    }
+
+    public void setState(CRMState newState) {
+        state = newState;
+    }
+
+    public void initState(String fileName) {
+        if (state == null) {
+            DBFileName = fileName;
+
+            CRMState loadedState = CRMStorage.load(fileName);
+            if (loadedState != null) {
+                state = loadedState; // set state to state loaded from file if it loads successfully.
+                System.out.println("Controller: Loaded CRM state from file: " + fileName);
+            } else {
+                state = new CRMState();
+                System.out.println("Controller: Initialized new CRM state. Failed to load from file: " + fileName);
+            }
+        } else {
+            System.out.println("Controller: CRM state is already initialized. Ignoring init command: ");
+        }
+    }
+
+    public String getDBFileName() {
+        return DBFileName;
+    }
+
+    public void setDBFileName(String fileName) {
+        DBFileName = fileName;
+    }
+
+    // execution related methods
+    public void run(String controlFileName) {
+        parser.parse(controlFileName);
+    }
+
+}
